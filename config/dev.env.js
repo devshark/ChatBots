5dcd96884fdcabf6a0e5e9459c8b6f092dbcb4ac
Anthony Lim <anthony.lim@transact24.com>
2018-01-19 15:32:06 +0800

    changed log level for dev env; added API info for fixer.io

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  LOG_LEVEL: '"info"'
+  LOG_LEVEL: '"debug"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,9 +1,11 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  LOG_LEVEL: '"error"',
   DISCORD_TOKEN: '""',
   DISCORD_INVITE: '""',
   API_WEATHER_ENDPOINT: '"https://api.openweathermap.org/data/2.5/weather"',
   API_WEATHER_KEY: '""',
-  LOG_LEVEL: '"error"'
+  API_FX_RATES_ENDPOINT: '"https://api.fixer.io/"',
+  API_FX_RATES_DEFAULT_CURRENCY: '"USD"'
 }
